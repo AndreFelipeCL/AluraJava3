a54a918d8c6bc47a33a5fc2eb347f032f9d736c6
AndreFelipeCL <andrefelipecl@outlook.com>
2018-06-12 22:54:18 -0300

    Commit inicial para a aula 3

---
+++ java3-bytebank/src/br/com/bytebankherdado/test/TesteReferencias.java
@@ -0,0 +1,30 @@
+package br.com.bytebankherdado.test;
+import br.com.bytebankherdado.controller.ControleBonificacao;
+import br.com.bytebankherdado.model.Designer;
+import br.com.bytebankherdado.model.EditorVideo;
+import br.com.bytebankherdado.model.Gerente;
+
+public class TesteReferencias {
+
+    public static void main(String[] args) {
+
+        Gerente g1 = new Gerente();
+        g1.setNome("Marcos");
+        g1.setSalario(5000.0);
+
+        EditorVideo ev = new EditorVideo();
+        ev.setSalario(2500.0);
+
+        Designer d = new Designer();
+        d.setSalario(2000.0);
+
+        ControleBonificacao controle = new ControleBonificacao();
+        controle.registra(g1);
+        controle.registra(ev);
+        controle.registra(d);
+
+        System.out.println(controle.getSoma());
+
+    }
+
+}
