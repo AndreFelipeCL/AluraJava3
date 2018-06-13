07c0278d2619c975b0ddfcb87ec323dc2c5facd3
AndreFelipeCL <andrefelipecl@outlook.com>
2018-06-12 23:05:47 -0300

    Conceito de herança com construtores para duas novas contas.

--- bytebank-herdado-conta/src/br/com/bytebankherdado/model/ContaCorrente.java
+++ bytebank-herdado-conta/src/br/com/bytebankherdado/model/ContaCorrente.java
@@ -1,8 +1,9 @@
 package br.com.bytebankherdado.model;
+
 public class ContaCorrente extends Conta {
 
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
 	}
-
+	
 }
---
+++ bytebank-herdado-conta/src/br/com/bytebankherdado/model/ContaPoupanca.java
@@ -0,0 +1,9 @@
+package br.com.bytebankherdado.model;
+
+public class ContaPoupanca extends Conta {
+
+	public ContaPoupanca(int agencia, int numero) {
+		super(agencia, numero);
+	}
+
+}
